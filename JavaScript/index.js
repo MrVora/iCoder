7b4ce9cd8f952ed6626b9efb534a9b95c833b643
Aagam <aagamvora2410@gmail.com>
2021-10-22 19:06:53 +0530

    Inital Commit

---
+++ JavaScript/index.js
@@ -0,0 +1,45 @@
+// if(browser == 'Edge'){
+//     alert("You've got the edge");
+// } else if (browser == 'Chrome' || 
+//     browser == 'Firefox' ||
+//     browser == 'Safari' ||
+//     browser == 'Opera' ){
+//         alert('Okay Okay');
+// } else {
+//     alert ('We Hope So!');
+// }
+
+
+// let age = prompt('Your age?', '');
+
+// switch (age) {
+//     case (age < 18):
+//         alert("Won't work"); // the result of prompt is a string, not a number
+//         break;
+
+//     case (age > 18):
+//         alert("This works!");
+//         break;
+
+//     default:
+//         alert("Any value not equal to one above");
+// }
+
+
+// function checkAge(age) {
+//     if (age >= 18) {
+//         return true;
+//     } else {
+//         return confirm('Do you have permission from your parents?');
+//     }
+// }
+
+// let age = prompt('How old are you?', '');
+
+// if (checkAge(age)) {
+//     alert('Access granted');
+// } else {
+//     alert('Access denied');
+// }
+
+    
